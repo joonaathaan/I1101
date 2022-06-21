ecf9d2903892005bf2548f632b04b7be28e0b0b7
jonathan <j.work.ah@gmail.com>
2022-06-21 18:24:41 +0300

    Ci: finish up ex11

--- Lab 3/exercise11.c
+++ Lab 3/exercise11.c
@@ -2,7 +2,7 @@
 #include <math.h>
 
 void main() {
-    int X, N, S = 0;
+    int X, N, S = 0, d = 1;
     do {
         printf("Input the value of x: ");
         scanf("%d", &X);
@@ -14,9 +14,12 @@ void main() {
     }
     while(N < 0);
     N *= 2;
+    printf("The values of series:\n");
     for(int i = 1; i < N; i+= 2 ){
-        if(i%4 == 1) S += pow(X * 1.0, i * 1.0);
-        else S -= pow(X * 1.0, i * 1.0);
+        int P =  d * pow(X, i);
+        S += P;
+        d *= -1;
+        printf("%d\n", P);
     }
-    printf("The sum of the series up to %d term is: %d\n", N, S);
+    printf("The sum of the series up to %d term is: %d\n", N / 2, S);
 }
